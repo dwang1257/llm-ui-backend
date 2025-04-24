14f8f28d73331f7c5a31420261e39836ce5b8a63
Dylan Wang <97680620+dwang1257@users.noreply.github.com>
2025-04-24 15:10:07 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+# Enable CORS for all routes
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello World"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
